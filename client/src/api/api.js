4abbd5ee7ec42d61b475eff9f3e89683d46e449c
OwenGranot <58481800+OwenGranot@users.noreply.github.com>
2021-05-01 16:35:06 +0300

    URIs, basic routes examples

---
+++ client/src/api/api.js
@@ -0,0 +1,34 @@
+import { ANOMALY_URI, MODEL_URI, MODELS_URI } from "./utils";
+
+/**
+ * @param {Object} data
+ * @param {String} type
+ * @returns {Object}
+ */
+export const addModel = async (data, type) => {
+  const config = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  };
+
+  const response = await fetch(MODEL_URI + `?model_type=${type}`, config);
+
+  return response.json();
+};
+
+/**
+ * @param {String} id
+ * @returns {Object}
+ */
+export const getModel = async (id) => {
+  const config = {
+    method: "GET",
+  };
+
+  const response = await fetch(MODEL_URI + `?model_id=${id}`, config);
+
+  return response.json();
+};

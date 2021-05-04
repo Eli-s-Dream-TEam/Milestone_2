70fe77c75a938f01f49278ae78d961c8b841ed37
OwenGranot <58481800+OwenGranot@users.noreply.github.com>
2021-05-04 16:15:40 +0300

    Placeholder, reading file

--- client/src/components/FileHandler/FileHandler.js
+++ client/src/components/FileHandler/FileHandler.js
@@ -2,8 +2,11 @@ import "./FileHandler.css";
 import { FileDrop } from "react-file-drop";
 import { useRef, useState } from "react";
 
+const TYPES = ["hybrid", "regression"];
+
 export default function FileHandler(props) {
   const [file, setFile] = useState(null);
+  const [type, setType] = useState(TYPES[0]);
   const uploaderRef = useRef(null);
 
   const removeActiveAndHover = () => {
@@ -26,13 +29,31 @@ export default function FileHandler(props) {
   const handleFileUpload = (files) => {
     removeActiveAndHover();
     const file = files[0];
+    const regexPattern = new RegExp(/.+\.csv$/);
 
     // No file
     if (!file) {
+      setFile(null);
       return;
     }
 
-    if (file.name) setFile(files[0]);
+    // Not a csv type
+    if (!file?.name.match(regexPattern)) {
+      setFile(null);
+      return;
+    }
+
+    setFile(files[0]);
+  };
+
+  const handleTrainModel = () => {
+    const reader = new FileReader();
+
+    reader.onload = () => {
+      console.table(reader.result);
+    };
+
+    reader.readAsBinaryString(file);
   };
 
   return (
@@ -64,13 +85,71 @@ export default function FileHandler(props) {
             Drop Train/Test File Here
           </label>
         </FileDrop>
+        {file && <hr style={{ width: "100%" }} />}
         <div>
           {file && (
-            <button className="btn btn-primary">
-              Train a model with this file
-            </button>
+            <div className="showcase-container">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                className="showcase-close-icon"
+                onClick={() => setFile(null)}
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+              <div>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="showcase-icon"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M9 17v-2m3 2v-4m3 4v-6m2 10H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
+                  />
+                </svg>
+                <p className="showcase-name">{file.name}</p>
+              </div>
+            </div>
           )}
-          {file && <div className="showcase">File Showcase</div>}
+          {file && (
+            <div>
+              <div className="select-container">
+                <label htmlFor="type">Model Type:</label>
+                <select
+                  type="select"
+                  name="type"
+                  onChange={(e) => setType(e.target.value)}
+                >
+                  {TYPES.map((type) => (
+                    <option key={`option-type-${type}`}>{type}</option>
+                  ))}
+                </select>
+              </div>
+              <div className="btn-container">
+                <button className="btn btn-primary" onClick={handleTrainModel}>
+                  Train
+                </button>
+                <button
+                  className="btn btn-secondary"
+                  onClick={handleTrainModel}
+                >
+                  Test
+                </button>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </>

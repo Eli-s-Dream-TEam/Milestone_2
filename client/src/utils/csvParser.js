12f9a92544383f6c0bae123042024c340d6e17f6
OwenGranot <58481800+OwenGranot@users.noreply.github.com>
2021-05-06 13:06:06 +0300

    Setting anomalies

--- client/src/utils/csvParser.js
+++ client/src/utils/csvParser.js
@@ -10,18 +10,17 @@ export default function parse(csvFile) {
       const parsedCsv = reader.result.split("\n");
       const features = parsedCsv[0].split(",");
       let data = {};
-      for(var i = 1; i < parsedCsv.length; i++) {
-        var mydata = parsedCsv[i].split(',');
-        for(var j = 0; j < mydata.length; j++) {
-          if (i == 1){
+      for (var i = 1; i < parsedCsv.length; i++) {
+        var mydata = parsedCsv[i].split(",");
+        for (var j = 0; j < mydata.length; j++) {
+          if (i === 1) {
             var lineData = [];
             var flo = parseFloat(mydata[j].trim());
             lineData.push(flo);
             data[features[j].trim()] = lineData;
-          }
-          else {
-              var floa = parseFloat(mydata[j].trim());
-              data[features[j].trim()].push(floa);
+          } else {
+            var floa = parseFloat(mydata[j].trim());
+            data[features[j].trim()].push(floa);
           }
         }
       }
